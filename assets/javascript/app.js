cf9a44128a5cefbfbd6bf6e734aaa3cf14461959
Eva Lieu <itsevalieu@gmail.com>
2016-10-29 17:10:33 -0700

    Semi pseudo-coded for app.js. Moved js additions to html for easier testing. Will move back to app.js later. Completed buttons and assign button values, completed search input, completed ajax part. Working on pausing gifs.

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+/*
+q - search query term or phrase
+limit - # of results to return, max 100, default 25
+rating - limit results to rated y, g, pg, pg-13 or r
+
+
+response.data.images.fixed_height.url
+response.data.rating
+*/
